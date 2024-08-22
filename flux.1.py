d18097495c73a8e60e254b6a772858b908d0fe05
Tarik Sghiouri idrissi <49345542+tariksghiouri@users.noreply.github.com>
2024-08-22 10:16:48 +0100

    flux 1 script

---
+++ flux.1.py
@@ -0,0 +1,17 @@
+import torch
+from diffusers import FluxPipeline
+
+pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
+
+prompt = "a fruit cake getting eaten by snails"
+image = pipe(
+    prompt,
+    height=1024,
+    width=1024,
+    guidance_scale=3.5,
+    num_inference_steps=50,
+    max_sequence_length=512,
+    generator=torch.Generator("cpu").manual_seed(0)
+).images[0]
+
+image.save("flux-dev.png")
